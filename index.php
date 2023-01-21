0cb0cdc0a6e706c5708f49295a822f646a6e379b
Adrien Gouacide <adrien.gouacide@gmail.com>
2023-01-21 20:27:07 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <a href="microsoftoauth.php" target="_blank">Sign in with microsoft</a>
+</body>
+</html>
---
+++ microsoftoauth.php
@@ -0,0 +1,7 @@
+<?php 
+
+$clientId = "ed194303-ed67-478c-9ce5-9c1de7eab56b";
+$tenantId = "common" ;
+$secretId = "2.w8Q~YKmdjncG6WTBBwQZxCecgcQNbEs_6Zvbw6";
+$redirectUri = "http://localhost:8080/microsoftoauth";
+$scopes = ['user.read', 'calendars.read'];
